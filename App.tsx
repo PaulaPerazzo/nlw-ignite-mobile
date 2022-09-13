bfe4dae26701ab8a46bbae64096b3c146b926db3
PaulaPerazzo <mpperazzosampaio@gmail.com>
2022-09-13 00:42:38 -0300

    mobile splash and home screen done

--- App.tsx
+++ App.tsx
@@ -1,46 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-
-interface ButtonProps {
-  title : string
-};
-
-function Button(props: ButtonProps) {
-  return (
-    <TouchableOpacity>
-      <Text>
-        {props.title}
-      </Text>
-    </TouchableOpacity>
-  );
-};
+import { StatusBar } from 'react-native';
+import { Background } from './src/components/Background';
+import {useFonts, 
+  Inter_400Regular, 
+  Inter_600SemiBold, 
+  Inter_700Bold, 
+  Inter_900Black} from '@expo-google-fonts/inter';
+import { Home } from './src/screens/Home';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular, 
+    Inter_600SemiBold, 
+    Inter_700Bold, 
+    Inter_900Black
+  });
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>
-        Yaaaaaaaaaaaay
-      </Text>
+    <Background >
+      <StatusBar
+      barStyle='light-content'
+      backgroundColor='transparent'
+      translucent/>
+      { fontsLoaded ?  <Home/> : <Loading/>}
 
-      <Button title='send 1'/>
-      <Button title='send 2'/>
-      <Button title='send 3'/>
-      <Button title='banana 99'/>
-
-      <StatusBar style="auto"/>
-    </View>
+    </Background>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#9887',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  title: {
-    fontSize: 25,
-  }
-});
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # nlw-ignite-mobile
 Rocketseat Nex Level Week mobile repo
+
+# technologies used:
+ - React-Native
+ - R-Component
+ - Reactive-native-safe-area-context
+ - expo-linear-gradient
